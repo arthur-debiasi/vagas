4ad52508ba68e7b29e1e91b335a9059b2486c522
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-06-11 11:25:11 -0300

    feat: DELETE /users reescrevendo fakeData.json e tratando erro

--- teste3.js
+++ teste3.js
@@ -1,15 +1,29 @@
-var data =  require("./fakeData");
+const data = require("./fakeData").fakeData;
+const { writeFile } = require("./utils/fileUtils");
 
-module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+module.exports = async function (req, res) {
+  const name = req.query.name;
+  let deleted = false;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name === name) {
+      data.splice(i, 1); // Remove o elemento do array na posição i
+      deleted = true;
+      break;
     }
+  }
 
-    res.send("success");
+  if (deleted) {
+    const filePath = "./fakeData.json";
+    const fileContent = JSON.stringify(data);
 
+    try {
+      await writeFile(filePath, fileContent);
+      res.sendStatus(204); // Retorna o status 204 No Content
+    } catch (err) {
+      res.status(500).json({ error: "Erro ao salvar o usuário" });
+    }
+  } else {
+    res.status(404).json({ error: "Usuário não encontrado" });
+  }
 };
