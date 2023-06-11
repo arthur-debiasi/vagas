d5ef825943fbb48dd991e38c05773cfd6474c5f9
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-06-11 10:58:07 -0300

    feat: implementa criação de novos usuários

--- teste2.js
+++ teste2.js
@@ -1,17 +1,27 @@
-var data =  require("./fakeData");
+const fs = require("fs");
+const path = require("path");
+const { fakeData } = require("./fakeData");
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
+module.exports = function (req, res) {
+    const name = req.body.name;
+    const job = req.body.job; // corrije "jov" para "job"
+
+    const newUser = {
+        id: fakeData.length + 1, // adiciona o id do usuário
         name: name,
         job: job,
-    }
+    };
 
-    data.push(newUser)
-    
+    fakeData.push(newUser);
+
+    const filePath = path.join(__dirname, "fakeData.json");
+    const fileContent = JSON.stringify(fakeData); // Converte o array de dados para formato JSON
+
+    fs.writeFile(filePath, fileContent, (error) => {
+        if (error) {
+            return res.status(500).json({ error: error.message }); // tratamento de erro
+        }
+
+    });
     res.send(newUser);
-
 };
