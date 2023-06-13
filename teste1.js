677883a4a62aabc2b9e2a6fced3365ffb8045edb
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-06-13 16:15:08 -0300

    fix: try/catch getUser

--- teste1.js
+++ teste1.js
@@ -1,27 +1,26 @@
-const { userFakeData: data } = require("./utils/data/fakeData");
-const path = require("path");
 const { readDataFromFile, writeDataToFile } = require('./utils/fileUtils');
 const { findUserByName } = require('./utils/findUserByName');
 
 const getUser = async (req, res) => {
-    const { name } = req.query;
-
-    const user = findUserByName(name, data);
-
-    if (!user) {
-        return res.status(404).json({ error: "User not found" });
-    }
-
-    if (!user.access) {
-        user.access = 1;
-    } else {
-        user.access += 1;
-    }
-
-    const userIndex = data.findIndex(({ name }) => name === user.name);
-    data[userIndex] = user;
-
     try {
+        const data = await readDataFromFile();
+        const { name } = req.query;
+
+        const user = findUserByName(name, data);
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        if (!user.access) {
+            user.access = 1;
+        } else {
+            user.access += 1;
+        }
+
+        const userIndex = data.findIndex(({ name }) => name === user.name);
+        data[userIndex] = user;
+
         await writeDataToFile(data);
         return res.status(200).json(user);
     } catch (err) {
@@ -29,6 +28,7 @@ const getUser = async (req, res) => {
         return res.status(500).json({ error: "Internal error" });
     }
 };
+
 
 const getUsers = async (_req, res) => {
     try {
